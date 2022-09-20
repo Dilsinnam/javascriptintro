0817d26ad4bd199b54b16f159772807a76a8c91a
Dilsin Amirtharajah <KillerzDaggers@Outlook.com>
2022-09-20 17:12:00 -0400

    Added constants, logs, and let statements

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-// String
-// 'Test' 'Test 2'
-// Numbers
-// 1321 4545 54.66
-// Booleans
-// true false
-// Empty
-// undefined null
+//alert("Hello World");
+console.log("Console Test"); 
+
+//var birthYear = 1984;
+//let isMad = false;
+//const birthYear = 1984; (Error)
+
+const birthYear = 1984;
+alert("Literally " + birthYear);
+
+let statement = "Sneery ";
+statement = statement + "is my username!";
+alert(statement);
